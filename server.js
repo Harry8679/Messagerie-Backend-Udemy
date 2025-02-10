c26567228f715ac5abeb280fe5ac409e3b0be798
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-10 23:41:19 +0100

    Setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // URL du frontend React
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Un utilisateur est connecté :", socket.id);
+
+  socket.on("send_message", (message) => {
+    io.emit("receive_message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Utilisateur déconnecté :", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Serveur WebSocket en écoute sur le port 5000");
+});
