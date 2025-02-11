6cb37d7e12f96418d13aea59a99404489963ba86
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-11 07:44:51 +0100

    Update

--- socket.js
+++ socket.js
@@ -1,4 +1,5 @@
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
+const io = require('../lib/socket.io');
 
 const socket = io("http://localhost:5000"); // URL de ton backend Socket.io
 export default socket;
